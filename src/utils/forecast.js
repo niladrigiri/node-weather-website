cafb209237ba99661c38cb5b600574d30ffd2b28
Niladri0777 <giriniladri5@gmail.com>
2019-09-11 20:16:03 +0530

    degree farenheight to celcius Add About text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     
-const url = 'https://api.darksky.net/forecast/56c074df6b818a2eb1c7acd125102f73/'+latitude+','+longitude
+const url = 'https://api.darksky.net/forecast/56c074df6b818a2eb1c7acd125102f73/'+latitude+','+longitude+'?units=si'
 request({ url, json: true }, (error , {body} = {}) => {
 
     if(error){
